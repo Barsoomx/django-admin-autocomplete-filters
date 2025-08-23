ef5524452d60df4ae5e2762511cbba54721e4673
Filipp <filipp@balakin.ru>
2025-08-23 11:37:01 +0300

    #76 #79

--- admin_auto_filters/views.py
+++ admin_auto_filters/views.py
@@ -13,7 +13,10 @@ class AutocompleteJsonView(Base):
         return str(obj)
 
     def get(self, request, *args, **kwargs):
-        self.term = request.GET.get('term', '')
+        if not hasattr(self, 'model_admin') and hasattr(self, 'process_request'):
+            self.term, self.model_admin, self.source_field, _ = self.process_request(request)
+        else:
+            self.term = request.GET.get('term', '')
         self.paginator_class = self.model_admin.paginator
         self.object_list = self.get_queryset()
         context = self.get_context_data()
@@ -24,3 +27,14 @@ class AutocompleteJsonView(Base):
             ],
             'pagination': {'more': context['page_obj'].has_next()},
         })
+
+
+    def get_queryset(self):
+        """Return queryset based on ModelAdmin.get_search_results()."""
+        qs = self.model_admin.get_queryset(self.request)
+        if hasattr(self.source_field, 'get_limit_choices_to'):
+            qs = qs.complex_filter(self.source_field.get_limit_choices_to())
+        qs, search_use_distinct = self.model_admin.get_search_results(self.request, qs, self.term)
+        if search_use_distinct:
+            qs = qs.distinct()
+        return qs
